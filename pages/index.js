0bc0ff302b2f7ef8d84e3794f34ce738801ab6a6
zunair <gillanix007@gmail.com>
2022-05-27 06:31:13 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -89,33 +89,7 @@ export default function Home({ posts }) {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 max-w-[1200px] mx-auto mb-8">
           {posts?.map((story, index) => (
             <div key={index} className="flex flex-col">
-              <Image
-                src={story.node.featuredImage.node.mediaItemUrl}
-                alt={story.node.title}
-                width={500}
-                height={350}
-              />
-              <div className="relative p-1 shadow-sm">
-                <div className="p-5 space-y-1">
-                  <h3 className="box-title">
-                    <Link href={story.node.uri}>{story.node.title}</Link>
-                  </h3>
-                  <p className="text-base font-medium text-[#535353]">
-                    {story.node.successStoryExtra.shortInfo}
-                  </p>
-                </div>
-                <div className="absolute bottom-0 right-0 flex justify-end">
-                  <figure className="max-h-[18px]">
-                    <Image
-                      src="/images/color-bar-light.jpg"
-                      alt="images/color-bar-light.jpg"
-                      className="w-[55%] h-[10px]"
-                      width={155}
-                      height={8}
-                    />
-                  </figure>
-                </div>
-              </div>
+              {story.node.title}
             </div>
           ))}
         </div>

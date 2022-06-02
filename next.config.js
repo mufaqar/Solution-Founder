ccb6694c6c91b22c7a17d1cf01b84e2cb5c4544e
zunair <gillanix007@gmail.com>
2022-06-02 08:14:19 -0700

    install svg plugin

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['solutionfounder.com'],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig

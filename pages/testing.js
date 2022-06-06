955de5ab261e958640aa6138f16fb475f5b2525f
zunair <gillanix007@gmail.com>
2022-06-06 01:21:04 -0700

    push fixing

--- pages/testing.js
+++ pages/testing.js
@@ -1,28 +1,7 @@
-import Stories from './components/Stories';
-import retail from '../public/images/retail.svg';
-import Image from 'next/image';
-const svgToImg = require("svg-to-img");
+import React from 'react'
 
-(async () => {
-  const image = await svgToImg.from("<svg xmlns='http://www.w3.org/2000/svg'/>").toPng();
-  
-  console.log(image);
-})();
-
-
-export default function HomeStories({ posts }) {
-
-
-
+export default function testing() {
   return (
-    <>
-      <div className="grid">
-        <h4>Testing</h4>
-        <Image src={retail}
-          width={200}
-          height={200}
-        ></Image>
-      </div>
-    </>
-  );
+    <div>testing</div>
+  )
 }

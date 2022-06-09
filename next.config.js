87f28cab265f5be7db0ae6b00c93ba0f91954da0
Mufaqar Islam <mufaqar@gmail.com>
2022-06-09 20:26:28 +0500

    Server Link Updated

--- next.config.js
+++ next.config.js
@@ -1,26 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withReactSvg = require('next-react-svg')
-const path = require('path')
-
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, 'src/assets/svg'),
-  webpack(config, options) {
-    return config
-  }
-})
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['solutionfounder.com'],
+    domains: ['anyposapp.com'],
   },
-  
-}
+};
 
 module.exports = nextConfig;
 
 {
   swcMinify: false; // it should be false by default
 }
-
-

1acda404795965671286d22035208ea91951331c
Mufaqar Islam <mufaqar@gmail.com>
2022-06-21 19:38:31 +0500

    Links

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,10 @@ import { gql } from '@apollo/client';
 import { client } from '../lib/apollo';
 import Link from 'next/link';
 import Image from 'next/image';
-import GlobalReach from '../public/images/Global-Reach.svg'
-import TrustedSecurity from '../public/images/Trusted-Security.svg'
-
+import GlobalReach from '../public/images/Global-Reach.svg';
+import TrustedSecurity from '../public/images/Trusted-Security.svg';
 
 export default function Home({ posts }) {
-  
   return (
     <>
       <Header />
@@ -83,58 +81,52 @@ export default function Home({ posts }) {
 
       <CTA />
 
-  
       <section className="py-28 px-7">
         <h2 className="md:text-4xl text-3xl leading-8 uppercase font-bold text-[#302E2E] text-center mb-8">
           OUR SUCCESS STORIES
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 max-w-[1200px] mx-auto mb-8">
-            {
-              posts.map((item, index) => (
-                <div key={index} className='flex flex-col'>
-                  <Image
-                    src={item.node.featuredImage.node.mediaItemUrl}
-                    alt={item.node.title}
-                    width={500}
-                    height={350}
-                  />
-                  <div className='relative p-1 shadow-sm'>
-                    <div className="p-5 space-y-1">
-                      <h3 className="box-title">
-                        <Link href={item.node.uri}>{item.node.title}</Link>
-                      </h3>
-                      <p className="text-base font-medium text-[#535353]">
-                        {item.node.successStoryExtra.shortInfo}
-                      </p>
-                    </div>
-                    <div className="absolute bottom-0 right-0 flex justify-end">
-                      <figure className="max-h-[18px]">
-                        <Image
-                          src="/images/color-bar-light.jpg"
-                          alt="images/color-bar-light.jpg"
-                          className="w-[55%] h-[10px]"
-                          width={155}
-                          height={8}
-                        />
-                      </figure>
-                    </div>
-                  </div>
+          {posts.map((item, index) => (
+            <div key={index} className="flex flex-col">
+              <Link href={item.node.uri}>
+                <Image
+                  src={item.node.featuredImage.node.mediaItemUrl}
+                  alt={item.node.title}
+                  width={500}
+                  height={350}
+                />
+              </Link>
+              <div className="relative p-1 shadow-sm">
+                <div className="p-5 space-y-1">
+                  <h3 className="box-title">
+                    <Link href={item.node.uri}>{item.node.title}</Link>
+                  </h3>
+                  <p className="text-base font-medium text-[#535353]">
+                    {item.node.successStoryExtra.shortInfo}
+                  </p>
                 </div>
-              ))
-            }
-          </div>
-
+                <div className="absolute bottom-0 right-0 flex justify-end">
+                  <figure className="max-h-[18px]">
+                    <Image
+                      src="/images/color-bar-light.jpg"
+                      alt="images/color-bar-light.jpg"
+                      className="w-[55%] h-[10px]"
+                      width={155}
+                      height={8}
+                    />
+                  </figure>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
-      
+
       <Footer />
     </>
   );
 }
-
-
-
-
 
 export async function getStaticProps() {
   const GET_POSTS = gql`
--- pages/industries-solution.js
+++ pages/industries-solution.js
@@ -8,9 +8,7 @@ import { client } from '../lib/apollo';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
-export default function IndustriesSolution({posts}) {
-
+export default function IndustriesSolution({ posts }) {
   return (
     <>
       <Header />
@@ -23,7 +21,7 @@ export default function IndustriesSolution({posts}) {
 
       <TabsSection />
 
-      <IntrustedDepartment/>
+      <IntrustedDepartment />
 
       <section className="py-28 px-7">
         <h2 className="md:text-4xl text-3xl leading-8 uppercase font-bold text-[#302E2E] text-center mb-8">
@@ -31,51 +29,46 @@ export default function IndustriesSolution({posts}) {
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 max-w-[1200px] mx-auto mb-8">
-            {
-              posts.map((item, index) => (
-                <div key={index} className='flex flex-col'>
-                  <Image
-                    src={item.node.featuredImage.node.mediaItemUrl}
-                    alt={item.node.title}
-                    width={500}
-                    height={350}
-                  />
-                  <div className='relative p-1 shadow-sm'>
-                    <div className="p-5 space-y-1">
-                      <h3 className="box-title">
-                        <Link href={item.node.uri}>{item.node.title}</Link>
-                      </h3>
-                      <p className="text-base font-medium text-[#535353]">
-                        {item.node.successStoryExtra.shortInfo}
-                      </p>
-                    </div>
-                    <div className="absolute bottom-0 right-0 flex justify-end">
-                      <figure className="max-h-[18px]">
-                        <Image
-                          src="/images/color-bar-light.jpg"
-                          alt="images/color-bar-light.jpg"
-                          className="w-[55%] h-[10px]"
-                          width={155}
-                          height={8}
-                        />
-                      </figure>
-                    </div>
-                  </div>
+          {posts.map((item, index) => (
+            <div key={index} className="flex flex-col">
+              <Link href={item.node.uri}>
+                <Image
+                  src={item.node.featuredImage.node.mediaItemUrl}
+                  alt={item.node.title}
+                  width={500}
+                  height={350}
+                />
+              </Link>
+              <div className="relative p-1 shadow-sm">
+                <div className="p-5 space-y-1">
+                  <h3 className="box-title">
+                    <Link href={item.node.uri}>{item.node.title}</Link>
+                  </h3>
+                  <p className="text-base font-medium text-[#535353]">
+                    {item.node.successStoryExtra.shortInfo}
+                  </p>
                 </div>
-              ))
-            }
-          </div>
-
+                <div className="absolute bottom-0 right-0 flex justify-end">
+                  <figure className="max-h-[18px]">
+                    <Image
+                      src="/images/color-bar-light.jpg"
+                      alt="images/color-bar-light.jpg"
+                      className="w-[55%] h-[10px]"
+                      width={155}
+                      height={8}
+                    />
+                  </figure>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
-      
+
       <Footer />
     </>
   );
 }
-
-
-
-
 
 export async function getStaticProps() {
   const GET_POSTS = gql`

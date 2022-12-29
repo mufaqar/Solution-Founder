b6a30b14233849a6e600282020abb99cda8e5d62
syed-zunair-gillani <gillanix007@gmail.com>
2022-12-29 19:54:36 +0500

    update single job

--- pages/career.js
+++ pages/career.js
@@ -1,51 +1,11 @@
 import Banner from './components/banner';
 import Footer from './components/footer';
 import Header from './components/header';
-import { GrClose } from 'react-icons/gr';
 import { gql } from '@apollo/client';
 import { client } from '../lib/apollo';
-import { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-import { motion } from 'framer-motion';
-import { useForm } from 'react-hook-form';
-import ConfermationMessage from './components/confermationMessage';
-import Loader from './components/Loader';
 import Link from 'next/link';
 
 export default function Career({ jobs }) {
-  console.log(jobs);
-  const [poupUp, setPopUp] = useState(false);
-  const form = useRef();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [loading, setLoding] = useState(false);
-  const [formStatus, setFormStatus] = useState(false);
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    setLoding(true);
-    emailjs
-      .sendForm(
-        'service_gvt30vq',
-        'template_es5tmhh',
-        e.target,
-        'pzMV1U0WIjGl0M4ye'
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          if (result.text === 'OK') {
-            setFormStatus(true);
-          }
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
 
   let counter = 1;
   return (
@@ -78,17 +38,12 @@ export default function Career({ jobs }) {
                   <div
                     dangerouslySetInnerHTML={{ __html: job.node.content }}
                   ></div>
-                  <p
-                    className="text-xl mt-4 font-bold text-[#133C6D] cursor-pointer"
-                    onClick={() => setPopUp(true)}
-                  >
-                    Apply Now
-                  </p>
+                 
                   <Link
-                    className="text-xl mt-4 font-bold text-[#133C6D] cursor-pointer"
                     href={job.node.uri}
                   >
-                    View Detail
+                  <p className="text-xl mt-4 font-bold text-[#133C6D] cursor-pointer">View Detail</p>
+                    
                   </Link>
                 </div>
               </div>
@@ -97,169 +52,7 @@ export default function Career({ jobs }) {
         </div>
       </section>
 
-      {/* Apply for job Popup */}
-      <section
-        className={`fixed top-0 bottom-0 left-0 right-0 z-50 bg-gray-700 bg-opacity-60 ${
-          poupUp ? 'block' : 'hidden'
-        }`}
-      >
-        {formStatus ? (
-          <ConfermationMessage />
-        ) : loading ? (
-          <Loader />
-        ) : (
-          <div className="fixed p-10 transform bg-white translate-x-1/2 -translate-y-1/2 top-1/2 right-1/2 w-[700px]">
-            <h2 className="text-4xl text-[#302E2E] font-bold">APPLY FOR Job</h2>
-            <h3 className="mt-2">SUBMIT YOUR APPLICATION</h3>
-
-            <form
-              className="mt-5"
-              ref={form}
-              onSubmit={handleSubmit(sendEmail)}
-            >
-              <div className="flex flex-wrap -m-2">
-                <div className="w-1/2 p-2">
-                  <div className="relative">
-                    <input
-                      type="text"
-                      name="firstName"
-                      {...register('firstName', { required: true })}
-                      placeholder="First Name"
-                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                    />
-                    {errors.FirstName && (
-                      <span className="block mt-1 text-red-400">
-                        First Name field is required<sup>*</sup>
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="w-1/2 p-2">
-                  <div className="relative">
-                    <input
-                      type="text"
-                      name="LastName"
-                      {...register('LastName', { required: true })}
-                      placeholder="Last Name"
-                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                    />
-                    {errors.FirstName && (
-                      <span className="block mt-1 text-red-400">
-                        field is required<sup>*</sup>
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex flex-wrap mt-2 -m-2">
-                <div className="w-1/2 p-2">
-                  <div className="relative">
-                    <input
-                      type="email"
-                      name="email"
-                      {...register('email', { required: true })}
-                      placeholder="Email"
-                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                    />
-                    {errors.FirstName && (
-                      <span className="block mt-1 text-red-400">
-                        field is required<sup>*</sup>
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="w-1/2 p-2">
-                  <div className="relative">
-                    <input
-                      type="number"
-                      name="Phone"
-                      {...register('Phone', { required: true })}
-                      placeholder="Phone"
-                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                    />
-                    {errors.FirstName && (
-                      <span className="block mt-1 text-red-400">
-                        field is required<sup>*</sup>
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex flex-wrap mt-2 -m-2">
-                <div className="w-1/2 p-2">
-                  <div className="relative">
-                    <input
-                      type="text"
-                      name="linkedinURL"
-                      {...register('linkedinURL')}
-                      placeholder="Linkedin URL"
-                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                    />
-                  </div>
-                </div>
-                <div className="w-1/2 p-2">
-                  <div className="relative">
-                    <input
-                      type="text"
-                      name="skypeID"
-                      {...register('skypeID')}
-                      placeholder="Skype ID"
-                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex flex-wrap mt-2 -m-2">
-                <div className="w-1/2 p-2">
-                  <div className="relative">
-                    <select
-                      className="block w-full px-3 py-2 m-0 text-base font-normal text-gray-700 transition ease-in-out bg-white bg-no-repeat border border-gray-300 border-solid rounded appearance-none form-select bg-clip-padding focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                      aria-label="Default select example"
-                      name="selectJob"
-                      {...register('selectJob', { required: true })}
-                    >
-                      {jobs.map((job) => (
-                        <option key={job.node.id}>{job.node.title}</option>
-                      ))}
-                    </select>
-                    {errors.FirstName && (
-                      <span className="block mt-1 text-red-400">
-                        field is required<sup>*</sup>
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="w-1/2 p-2">
-                  <div className="relative">
-                    <input
-                      type="file"
-                      name="cv"
-                      {...register('selectJob')}
-                      placeholder="Skype ID"
-                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <input
-                type="submit"
-                value="Submit Form"
-                className="bg-[#8DC63E] text-white font-bold hover:bg-gray-800 mt-5 px-4 py-2"
-              />
-            </form>
-
-            <GrClose
-              size={18}
-              className="absolute cursor-pointer right-2 top-2"
-              onClick={() => setPopUp(false)}
-            />
-          </div>
-        )}
-      </section>
+     
 
       <Footer />
     </div>
--- pages/jobs/[uri].js
+++ pages/jobs/[uri].js
@@ -1,16 +1,51 @@
-import Head from 'next/head';
-import { client } from '../../lib/apollo';
-import { gql } from '@apollo/client';
-import Header from '../components/header';
-import Banner from '../components/banner';
-import Cardnew from '../components/card-new';
-import IntrustedDepartment from '../components/intrusted-department';
-import Footer from '../components/footer';
-import Link from 'next/link';
-import Image from 'next/image';
+import Head from "next/head";
+import { client } from "../../lib/apollo";
+import { gql } from "@apollo/client";
+import Header from "../components/header";
+import Banner from "../components/banner";
+import Footer from "../components/footer";
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { useForm } from 'react-hook-form';
+import ConfermationMessage from '../components/confermationMessage';
+import Loader from '../components/Loader';
+import { GrClose } from 'react-icons/gr';
 
-export default function SlugPage({ job, successStories }) {
-  console.log('story Banner', job);
+
+export default function SlugPage({ job,jobs, successStories }) {
+  console.log("story Banner", job);
+  const [poupUp, setPopUp] = useState(false);
+  const form = useRef();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [loading, setLoding] = useState(false);
+  const [formStatus, setFormStatus] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoding(true);
+    emailjs
+      .sendForm(
+        "service_gvt30vq",
+        "template_es5tmhh",
+        e.target,
+        "pzMV1U0WIjGl0M4ye"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if (result.text === "OK") {
+            setFormStatus(true);
+          }
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <div>
@@ -21,9 +56,185 @@ export default function SlugPage({ job, successStories }) {
 
       <Header />
 
-      
+      <Banner
+        heading={job.title}
+        // sub_heading="Create a world where anyone can belong anywhere, Join the Team"
+        icon="career-banner-img.png"
+      />
 
-      
+      <section className="max-w-[1200px] _singlejob px-6 mx-auto">
+        <div dangerouslySetInnerHTML={{ __html: job.content }}></div>
+        <p
+          className="text-xl mt-4 font-bold underline text-[#133C6D] cursor-pointer"
+          onClick={() => setPopUp(true)}
+        >
+          Apply Now
+        </p>
+      </section>
+
+      {/* Apply for job Popup */}
+      <section
+        className={`fixed top-0 bottom-0 left-0 right-0 z-50 bg-gray-700 bg-opacity-60 ${
+          poupUp ? "block" : "hidden"
+        }`}
+      >
+        {formStatus ? (
+          <ConfermationMessage />
+        ) : loading ? (
+          <Loader />
+        ) : (
+          <div className="fixed p-10 transform bg-white translate-x-1/2 -translate-y-1/2 top-1/2 right-1/2 w-[700px]">
+            <h2 className="text-4xl text-[#302E2E] font-bold">APPLY FOR Job</h2>
+            <h3 className="mt-2">SUBMIT YOUR APPLICATION</h3>
+
+            <form
+              className="mt-5"
+              ref={form}
+              onSubmit={handleSubmit(sendEmail)}
+            >
+              <div className="flex flex-wrap -m-2">
+                <div className="w-1/2 p-2">
+                  <div className="relative">
+                    <input
+                      type="text"
+                      name="firstName"
+                      {...register("firstName", { required: true })}
+                      placeholder="First Name"
+                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
+                    />
+                    {errors.FirstName && (
+                      <span className="block mt-1 text-red-400">
+                        First Name field is required<sup>*</sup>
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <div className="w-1/2 p-2">
+                  <div className="relative">
+                    <input
+                      type="text"
+                      name="LastName"
+                      {...register("LastName", { required: true })}
+                      placeholder="Last Name"
+                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
+                    />
+                    {errors.FirstName && (
+                      <span className="block mt-1 text-red-400">
+                        field is required<sup>*</sup>
+                      </span>
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-wrap mt-2 -m-2">
+                <div className="w-1/2 p-2">
+                  <div className="relative">
+                    <input
+                      type="email"
+                      name="email"
+                      {...register("email", { required: true })}
+                      placeholder="Email"
+                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
+                    />
+                    {errors.FirstName && (
+                      <span className="block mt-1 text-red-400">
+                        field is required<sup>*</sup>
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <div className="w-1/2 p-2">
+                  <div className="relative">
+                    <input
+                      type="number"
+                      name="Phone"
+                      {...register("Phone", { required: true })}
+                      placeholder="Phone"
+                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
+                    />
+                    {errors.FirstName && (
+                      <span className="block mt-1 text-red-400">
+                        field is required<sup>*</sup>
+                      </span>
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-wrap mt-2 -m-2">
+                <div className="w-1/2 p-2">
+                  <div className="relative">
+                    <input
+                      type="text"
+                      name="linkedinURL"
+                      {...register("linkedinURL")}
+                      placeholder="Linkedin URL"
+                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
+                    />
+                  </div>
+                </div>
+                <div className="w-1/2 p-2">
+                  <div className="relative">
+                    <input
+                      type="text"
+                      name="skypeID"
+                      {...register("skypeID")}
+                      placeholder="Skype ID"
+                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-wrap mt-2 -m-2">
+                <div className="w-1/2 p-2">
+                  <div className="relative">
+                    <select
+                      className="block w-full px-3 py-2 m-0 text-base font-normal text-gray-700 transition ease-in-out bg-white bg-no-repeat border border-gray-300 border-solid rounded appearance-none form-select bg-clip-padding focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      aria-label="Default select example"
+                      name="selectJob"
+                      {...register("selectJob", { required: true })}
+                    >
+                      {jobs.map((job) => (
+                        <option key={job.node.id}>{job.node.title}</option>
+                      ))}
+                    </select>
+                    {errors.FirstName && (
+                      <span className="block mt-1 text-red-400">
+                        field is required<sup>*</sup>
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <div className="w-1/2 p-2">
+                  <div className="relative">
+                    <input
+                      type="file"
+                      name="cv"
+                      {...register("selectJob")}
+                      placeholder="Skype ID"
+                      className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 outline-none focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <input
+                type="submit"
+                value="Submit Form"
+                className="bg-[#8DC63E] text-white font-bold hover:bg-gray-800 mt-5 px-4 py-2"
+              />
+            </form>
+
+            <GrClose
+              size={18}
+              className="absolute cursor-pointer right-2 top-2"
+              onClick={() => setPopUp(false)}
+            />
+          </div>
+        )}
+      </section>
 
       <Footer />
     </div>
@@ -39,6 +250,16 @@ const GET_POST = gql`
       uri
     }
 
+    jobs {
+      edges {
+        node {
+          uri
+          id
+          title
+          content
+        }
+      }
+    }
 
     allSuccessStories(first: 4) {
       edges {
@@ -60,7 +281,7 @@ const GET_POST = gql`
 `;
 
 export async function getStaticProps({ params }) {
-  console.log('params',params)
+  console.log("params", params);
   //  the params argument for this function corresponds to the dynamic URL segments
   //  we included in our page-based route. So, in this case, the `params` object will have
   //  a property named `uri` that contains that route segment when a user hits the page
@@ -73,9 +294,11 @@ export async function getStaticProps({ params }) {
   // const response = await getPostByUri(params.uri)
   const job = response?.data?.job;
   const successStories = response.data.allSuccessStories.edges;
+  const jobs = response?.data?.jobs?.edges;
   return {
     props: {
       job,
+      jobs,
       successStories,
     },
   };
@@ -85,6 +308,6 @@ export async function getStaticPaths() {
   const paths = [];
   return {
     paths,
-    fallback: 'blocking',
+    fallback: "blocking",
   };
 }

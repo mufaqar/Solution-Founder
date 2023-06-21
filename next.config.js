a9300bcd07ff2f477f4404b7c70fc172d0502986
mufaqar <mufaqar@gmail.com>
2023-06-21 16:36:07 +0500

    SVG

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
   images: {
     domains: ['sf.tmg.mt'],
   },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+        },
+      },
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;

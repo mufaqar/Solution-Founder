a056f8cc8dd0b1be2cf4e4485d57194bc01e3d85
mufaqar <mufaqar@gmail.com>
2023-06-21 18:03:17 +0500

    new config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['sf.tmg.mt'],
     dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };

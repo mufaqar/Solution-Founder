64b4fe9a1ba3b8cd2fccb527b99e6fc606be34a5
syed zunair gillani <gillanix007@gmail.com>
2023-06-21 19:02:26 +0500

    update

--- next.config.js
+++ next.config.js
@@ -3,8 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
+    domains: ["sf.tmg.mt"],
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
 
